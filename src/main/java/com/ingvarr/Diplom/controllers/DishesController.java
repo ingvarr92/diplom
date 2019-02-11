1cbb43b5955ea94f6651b2509bcf5264f9ed5dde
Igor <ingvarr92@gmail.com>
2019-02-11 21:25:23 +0300

    Создание заказа

--- src/main/java/com/ingvarr/Diplom/controllers/DishesController.java
+++ src/main/java/com/ingvarr/Diplom/controllers/DishesController.java
@@ -20,9 +20,9 @@ public class DishesController {
     @Autowired
     protected OrdersRepository ordersRepository;
 
-    @RequestMapping(name = "/")
-    public String index(@ModelAttribute Dishes dish, Model model){
 
+    @RequestMapping(name = "/")
+    public String index(@ModelAttribute Dishes dish, Model model, Orders orders){
         List<Dishes> dishes = (List<Dishes>) dishesRepository.findAll();
         model.addAttribute("dishes", dishes);
 
@@ -30,6 +30,17 @@ public class DishesController {
         model.addAttribute("orderdishes",orderdishes);
         return "index.html";
     }
+
+//    @RequestMapping(name = "/add_tbl/{tbl}")
+//    public String setTbl(@PathVariable("tbl") Integer tbl, @ModelAttribute Dishes dish, Model model, Orders order){
+//        order.setTable(tbl);
+//        List<Dishes> dishes = (List<Dishes>) dishesRepository.findAll();
+//        model.addAttribute("dishes", dishes);
+//
+//        List<Orders> orderdishes = (List<Orders>) ordersRepository.findAll();//(List<Dishes>) ordersRepository.findAll();
+//        model.addAttribute("orderdishes",orderdishes);
+//        return "index.html";
+//    }
 
     @RequestMapping("/admin")
     public String admin(){
@@ -55,16 +66,20 @@ public class DishesController {
     }
 
     @RequestMapping("/dishes")
-    public String dishesList(){
+    public String dishesList(@ModelAttribute Dishes dish, Model model){
         List<Dishes> dishes = (List<Dishes>) dishesRepository.findAll();
+        model.addAttribute("dishes", dishes);
         return "dish_list.html";
     }
 
     @RequestMapping("/add_dish/{id}")
-    public String addProductToCart(@PathVariable("id") Integer dishId) {
+    public String addProductToCart(@PathVariable("id") Integer dishId, @ModelAttribute Orders order) {
         System.out.println("DishID "+ dishId);
-        Orders order = new Orders();
-        order.setPrice(dishesRepository.findById(dishId).get().getPrice());
+        if (order.getPrice()==null) {
+            order.setPrice(dishesRepository.findById(dishId).get().getPrice());
+        }else {
+            order.setPrice(dishesRepository.findById(dishId).get().getPrice() + order.getPrice());
+        }
         order.setStatus("Создан");
         order.setTable(5);
         order.getDishes().add(dishesRepository.findById(dishId).get());
